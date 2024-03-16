4e27d3835586b539908b5f22278cdf255b8a0bad
mahdigh621 <mahdigh621@gmail.com>
2024-03-17 02:34:31 +0330

    Navbar finish!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import Shop from "./Pages/Shop";
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+library.add(fas, faTwitter, faFontAwesome)
+
 
 function App() {
   return (
     <div >
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path="/HomeProducts" element={<ShopCategory category="HomeProducts" />}></Route>
+
+          <Route path="/MedicalProducts" element={<ShopCategory category="MedicalProducts" />}></Route>
+          <Route path="/IndustryProducts" element={<ShopCategory category="IndustryProducts" />}></Route>
+
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId"></Route>
+
+          </Route>
+          <Route path="/Cart" element={<Cart />}></Route>
+          <Route path="/Login" element={<LoginSignup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Logo from '../Assets/لوگو تستی.png'
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
+	const [menu, setMenu] = useState('Home')
 	return (
-		<div>Navbar</div>
+		<div className="navbar">
+			<div className="nav-logo">
+				<img src={Logo} alt="Logo" />
+			</div>
+			<ul className="nav-menu">
+				<li onClick={() => { setMenu("Home") }}><Link style={{ textDecoration: 'none' }} to={"/"}>خانه</Link>{menu === "Home" ? <hr /> : <></>}</li>
+				<li onClick={() => { setMenu("HomePro") }}><Link style={{ textDecoration: 'none' }} to={"/HomeProducts"}>خانگی</Link>{menu === "HomePro" ? <hr /> : <></>}</li>
+				<li onClick={() => { setMenu("MedPro") }}> <Link style={{ textDecoration: 'none' }} to={"/MedicalProducts"}>بهداشتی</Link> {menu === "MedPro" ? <hr /> : <></>}</li>
+				<li onClick={() => { setMenu("indPro") }}> <Link style={{ textDecoration: 'none' }} to={"/IndustryProducts"}>صنعتی</Link>{menu === "indPro" ? <hr /> : <></>}</li>
+			</ul>
+			<div className="nav-login-cart">
+				<Link to={"/Login"}> <button>ورود</button> </Link>
+				<Link to={"/Cart"}> <FontAwesomeIcon className='cart' icon="fa-solid fa-cart-shopping" /> </Link>
+				<div className="nav-cart-count">0</div>
+			</div>
+
+		</div>
 	)
 }
